e64e8d4936a1809cd1e2f711541973f19b68e35b
oakinogundeji <oakinogundeji@gmail.com>
2018-02-16 17:01:11 +0100

    package: extracted event-controller getSelection into helpers module

--- event-controller.js
+++ event-controller.js
@@ -13,70 +13,13 @@ const
   moment = require('moment'),
   DBURL = process.env.DBURL,
   EventCardModel = require('./models/event-cards'),
+  getSelections = require('./helpers').getSelections,
   SMARKETS_URL = process.env.SMARKETS_URL,
   SMARKETS_EVENTS_CONTAINER_SELECTOR = 'ul.contracts',
   SMARKETS_SELECTIONS_SELECTOR = 'div.contract-info';
 
 let selectionsList;
 // helper functions
-
-async function getSelections() {
-  // setup
-  let
-    sport,
-    flag;
-  const URL_ARR = SMARKETS_URL.split('/');
-  sport = URL_ARR[6];
-  if(sport == 'horse-racing' ) {
-    flag = 'HR';
-  } else {
-    flag = 'GENERIC';
-  }
-  console.log(`sport: ${sport}...`);
-  // instantiate browser
-  const browser = await P.launch({
-    headless: false,
-    timeout: 180000
-  });
-  // create blank page
-  const page = await browser.newPage();
-  // set viewport to 1366*768
-  await page.setViewport({width: 1366, height: 768});
-  // set the user agent
-  await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)');
-  await page.goto(SMARKETS_URL, {
-    waitUntil: 'networkidle2',
-    timeout: 180000
-  });
-  await page.waitFor(10*1000);
-  // ensure race container selector available
-  await page.waitForSelector(SMARKETS_EVENTS_CONTAINER_SELECTOR);
-  // allow 'page' instance to output any calls to browser log to node log
-  page.on('console', data => console.log(data.text()));
-  console.log('SMARKETS_EVENTS_CONTAINER_SELECTOR found, continuing...');
-  // get list of selections
-  selectionsList = await page.$$eval(SMARKETS_SELECTIONS_SELECTOR, (targets, flag) => {
-    let selectionsList = [];
-    if(flag == 'HR') {
-      targets.filter(target => {
-        if(target.parentElement.nextElementSibling.children[0].children[0].className == 'price-section') {
-          const selection = target.children[1].children[0].innerText;
-          console.log(`selection info for HR: ${selection}`);
-          return selectionsList.push(selection);
-        }
-      });
-    } else {
-      targets.forEach(target => {
-        const selection = target.innerText;
-        console.log(`selection info for GENERIC: ${selection}`);
-        return selectionsList.push(selection);
-      });
-    }
-    return selectionsList;
-  }, flag);
-  await browser.close();
-  return Promise.resolve(true);
-}
 
 async function createEventCard() {
 
@@ -176,9 +119,10 @@ function connectToDB() {
 connectToDB()
   .then(ok => {
     console.log('getting selections...');
-    return getSelections();
+    return getSelections(P, SMARKETS_URL, SMARKETS_EVENTS_CONTAINER_SELECTOR, SMARKETS_SELECTIONS_SELECTOR);
   })
-  .then(ok => {
+  .then(data => {
+    selectionsList = data;
     console.log('selectionsList...');
     console.log(selectionsList);
     return Promise.resolve(true);
